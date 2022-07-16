f93fccbb2573d26ce26580bcd61735f74d5ce8af
Reginald McLean <rbmclean00@gmail.com>
2022-07-16 09:12:11 -0500

    Actually allow noop-logging

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 #include "logger.h"
 
+#ifndef DISABLE_LOGGING
+
 static unsigned char logLevel = 0;
 
 void setLogLevel(unsigned char level){
-	#ifndef DISABLE_LOGGING
 	logLevel = level;
-	#endif
 }
 
 //if producing the msg might be expensive, this can be called
 //to determine whether to produce the msg, followed by calling log()
 bool wouldLog(unsigned char level){
-	#ifndef DISABLE_LOGGING
 	return level <= logLevel;
-	#else
-	return false;
-	#endif
 }
 
 void log(unsigned char level, std::string msg, bool error){
-	#ifndef DISABLE_LOGGING
 	if(level <= logLevel){
 		if(error)
 			std::cerr << msg;
 		else
 			std::cout << msg;
 	}
-	#endif
 }
+
+#endif
--- src/logger.h
+++ src/logger.h
@@ -1,5 +1,15 @@
 #include <string>
 
+#ifndef DISABLE_LOGGING
+
 void setLogLevel(unsigned char level);
 bool wouldLog(unsigned char level);
 void log(unsigned char level, std::string msg, bool error = false);
+
+#else
+
+#define setLogLevel(level) (void)0
+#define wouldLog(level) false
+#define log(...) (void)0
+
+#endif
